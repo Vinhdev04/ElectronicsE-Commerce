0dbe57c8ff809f977fa46a6650f1f7a21238f01d
Phạm Công Vinh <pcv.fed@gmail.com>
2025-02-17 02:51:44 +0700

    Update code Banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ const swiper = new Swiper(".card-swiper", {
   loop: true, // Enable loop if needed
   slidesPerView: "auto", // Adjust to show multiple slides at once
   spaceBetween: 10, // Space between slides
+  // autoplay
+  autoplay: {
+    delay: 3000,
+  },
 
   navigation: {
     nextEl: ".swiper-button-next",
@@ -25,6 +29,41 @@ const swiper = new Swiper(".card-swiper", {
     },
     1024: {
       slidesPerView: 3,
+    },
+  },
+});
+
+const swiperBanner = new Swiper(".banner-swiper", {
+  loop: true, // Enable loop if needed
+  slidesPerView: "auto", // Adjust to show multiple slides at once
+  spaceBetween: 10, // Space between slides
+  // autoplay
+  autoplay: {
+    delay: 3000,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+
+    dynamicBullets: true,
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 1,
     },
   },
 });
