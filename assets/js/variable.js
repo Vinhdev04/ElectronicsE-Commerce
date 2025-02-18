3ef0976b78a17e8dc0dec706119dc6d3062d54a3
Phạm Công Vinh <pcv.fed@gmail.com>
2025-02-19 01:58:51 +0700

    Update code Product[Tablet]

--- assets/js/variable.js
+++ assets/js/variable.js
@@ -1,11 +1,15 @@
 //TODO: HANDLE SHOW LOGIN/SIGNUP
 export const isShowSignIn = document.getElementById("sign-in");
-console.log(isShowSignIn);
+// console.log(isShowSignIn);
 export const isShowSignUp = document.getElementById("sign-up");
-console.log(isShowSignUp);
+// console.log(isShowSignUp);
 export const btnCreateAccount = document.getElementById("btn-createAccount");
-console.log(btnCreateAccount);
+// console.log(btnCreateAccount);
 export const btnSignUp = document.getElementById("btn-Signup");
-console.log(btnSignUp);
+// console.log(btnSignUp);
 export const signUp = document.getElementById("sign-Up");
-console.log(signUp);
+// console.log(signUp);
+// export const filterButtons = document.querySelectorAll(".filter__branch");
+// // console.log("🚀 ~ filterButtons :", filterButtons);
+// export const products = document.querySelectorAll(".product");
+// // console.log("🚀 ~ products :", products);
