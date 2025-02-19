e2f95a9d7c555de9074e353bd3c659efcb873702
Phạm Công Vinh <pcv.fed@gmail.com>
2025-02-19 15:42:43 +0700

    Update code Filter Product[Watch,Laptop,SmartPhone,Tablet]

--- assets/js/variable.js
+++ assets/js/variable.js
@@ -1,15 +1,6 @@
 //TODO: HANDLE SHOW LOGIN/SIGNUP
 export const isShowSignIn = document.getElementById("sign-in");
-// console.log(isShowSignIn);
 export const isShowSignUp = document.getElementById("sign-up");
-// console.log(isShowSignUp);
 export const btnCreateAccount = document.getElementById("btn-createAccount");
-// console.log(btnCreateAccount);
 export const btnSignUp = document.getElementById("btn-Signup");
-// console.log(btnSignUp);
 export const signUp = document.getElementById("sign-Up");
-// console.log(signUp);
-// export const filterButtons = document.querySelectorAll(".filter__branch");
-// // console.log("🚀 ~ filterButtons :", filterButtons);
-// export const products = document.querySelectorAll(".product");
-// // console.log("🚀 ~ products :", products);
