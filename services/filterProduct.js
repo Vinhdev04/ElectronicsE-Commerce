0675fcd5e23ed32ec455e54c6217ca5916f24ad7
Phạm Công Vinh <pcv.fed@gmail.com>
2025-02-23 01:53:33 +0700

    Update remove comment filter Category

--- services/filterProduct.js
+++ services/filterProduct.js
@@ -1,99 +1,99 @@
-// //TODO: fILTER PRODUCT[TABLET] BY CATEGORY
-// document.addEventListener("DOMContentLoaded", function () {
-//   const filterButtons = document.querySelectorAll("[data-filter-tablet]");
-//   const products = document.querySelectorAll(".product-tablet");
+//TODO: fILTER PRODUCT[TABLET] BY CATEGORY
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll("[data-filter-tablet]");
+  const products = document.querySelectorAll(".product-tablet");
 
-//   filterButtons.forEach((button) => {
-//     button.addEventListener("click", function () {
-//       const selectedCategory = this.getAttribute("data-filter-tablet");
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const selectedCategory = this.getAttribute("data-filter-tablet");
 
-//       products.forEach((product) => {
-//         const productCategory = product.getAttribute("data-category");
+      products.forEach((product) => {
+        const productCategory = product.getAttribute("data-category");
 
-//         if (
-//           selectedCategory === "all" ||
-//           productCategory === selectedCategory
-//         ) {
-//           product.closest(".col-md-4").style.display = "flex"; // Hiển thị sản phẩm
-//         } else {
-//           product.closest(".col-md-4").style.display = "none"; // Ẩn sản phẩm
-//         }
-//       });
-//     });
-//   });
-// });
+        if (
+          selectedCategory === "all" ||
+          productCategory === selectedCategory
+        ) {
+          product.closest(".col-md-4").style.display = "flex"; // Hiển thị sản phẩm
+        } else {
+          product.closest(".col-md-4").style.display = "none"; // Ẩn sản phẩm
+        }
+      });
+    });
+  });
+});
 
-// //TODO: fILTER PRODUCT[SMART-PHONE] BY CATEGORY
-// document.addEventListener("DOMContentLoaded", function () {
-//   const filterButtons = document.querySelectorAll("[data-filter-smart]");
-//   const products = document.querySelectorAll(".product-smart");
+//TODO: fILTER PRODUCT[SMART-PHONE] BY CATEGORY
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll("[data-filter-smart]");
+  const products = document.querySelectorAll(".product-smart");
 
-//   filterButtons.forEach((button) => {
-//     button.addEventListener("click", function () {
-//       const selectedCategory = this.getAttribute("data-filter-smart");
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const selectedCategory = this.getAttribute("data-filter-smart");
 
-//       products.forEach((product) => {
-//         const productCategory = product.getAttribute("data-category");
+      products.forEach((product) => {
+        const productCategory = product.getAttribute("data-category");
 
-//         if (
-//           selectedCategory === "all" ||
-//           productCategory === selectedCategory
-//         ) {
-//           product.closest(".col-md-4").style.display = "flex"; // Hiển thị sản phẩm
-//         } else {
-//           product.closest(".col-md-4").style.display = "none"; // Ẩn sản phẩm
-//         }
-//       });
-//     });
-//   });
-// });
+        if (
+          selectedCategory === "all" ||
+          productCategory === selectedCategory
+        ) {
+          product.closest(".col-md-4").style.display = "flex"; // Hiển thị sản phẩm
+        } else {
+          product.closest(".col-md-4").style.display = "none"; // Ẩn sản phẩm
+        }
+      });
+    });
+  });
+});
 
-// //TODO: fILTER PRODUCT[LAPTOP] BY CATEGORY
-// document.addEventListener("DOMContentLoaded", function () {
-//   const filterButtons = document.querySelectorAll("[data-filter-laptop]");
-//   const products = document.querySelectorAll(".product-laptop");
+//TODO: fILTER PRODUCT[LAPTOP] BY CATEGORY
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll("[data-filter-laptop]");
+  const products = document.querySelectorAll(".product-laptop");
 
-//   filterButtons.forEach((button) => {
-//     button.addEventListener("click", function () {
-//       const selectedCategory = this.getAttribute("data-filter-laptop");
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const selectedCategory = this.getAttribute("data-filter-laptop");
 
-//       products.forEach((product) => {
-//         const productCategory = product.getAttribute("data-category");
+      products.forEach((product) => {
+        const productCategory = product.getAttribute("data-category");
 
-//         if (
-//           selectedCategory === "all" ||
-//           productCategory === selectedCategory
-//         ) {
-//           product.closest(".col-md-4").style.display = "flex"; // Hiển thị sản phẩm
-//         } else {
-//           product.closest(".col-md-4").style.display = "none"; // Ẩn sản phẩm
-//         }
-//       });
-//     });
-//   });
-// });
+        if (
+          selectedCategory === "all" ||
+          productCategory === selectedCategory
+        ) {
+          product.closest(".col-md-4").style.display = "flex"; // Hiển thị sản phẩm
+        } else {
+          product.closest(".col-md-4").style.display = "none"; // Ẩn sản phẩm
+        }
+      });
+    });
+  });
+});
 
-// //TODO: fILTER PRODUCT[WATCH] BY CATEGORY
-// document.addEventListener("DOMContentLoaded", function () {
-//   const filterButtons = document.querySelectorAll("[data-filter-watch]");
-//   const products = document.querySelectorAll(".product-watch");
+//TODO: fILTER PRODUCT[WATCH] BY CATEGORY
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll("[data-filter-watch]");
+  const products = document.querySelectorAll(".product-watch");
 
-//   filterButtons.forEach((button) => {
-//     button.addEventListener("click", function () {
-//       const selectedCategory = this.getAttribute("data-filter-watch");
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const selectedCategory = this.getAttribute("data-filter-watch");
 
-//       products.forEach((product) => {
-//         const productCategory = product.getAttribute("data-category");
+      products.forEach((product) => {
+        const productCategory = product.getAttribute("data-category");
 
-//         if (
-//           selectedCategory === "all" ||
-//           productCategory === selectedCategory
-//         ) {
-//           product.closest(".col-md-4").style.display = "flex"; // Hiển thị sản phẩm
-//         } else {
-//           product.closest(".col-md-4").style.display = "none"; // Ẩn sản phẩm
-//         }
-//       });
-//     });
-//   });
-// });
+        if (
+          selectedCategory === "all" ||
+          productCategory === selectedCategory
+        ) {
+          product.closest(".col-md-4").style.display = "flex"; // Hiển thị sản phẩm
+        } else {
+          product.closest(".col-md-4").style.display = "none"; // Ẩn sản phẩm
+        }
+      });
+    });
+  });
+});
