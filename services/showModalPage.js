91879f7f445d1f1322e989238799fcf095ae445c
Phạm Công Vinh <pcv.fed@gmail.com>
2025-02-22 23:47:30 +0700

    Update code Modal Home Page

---
+++ services/showModalPage.js
@@ -0,0 +1,5 @@
+//TODO SHOW MODAL OPENING WHEN IS OPENING WEBSITE
+document.addEventListener("DOMContentLoaded", function () {
+  var myModal = new bootstrap.Modal(document.getElementById("welcomeModal"));
+  myModal.show();
+});
