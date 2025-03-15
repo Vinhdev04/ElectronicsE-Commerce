232135c634c64a44ee46828f1c166872752c0b18
Phạm Công Vinh <pcv.fed@gmail.com>
2025-03-16 02:34:08 +0700

    Update code Mega menu - Test 02

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
 //TODO: LOAD CODE[SUB-MENU] FROME FILE "navbar.html" to "index.html"
 //TODO: LOAD CODE[FOOTER] FROME FILE "footer.html" to "index.html"
 //TODO: LOAD CODE[NAVBAR] FROME FILE "navbar.html" to "index.html"
-function loadHTML(elementId, file) {
-  fetch(file)
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById(elementId).innerHTML = data;
-    })
-    .catch((error) => alert("Lỗi trong quá trình tải và đọc dữ liệu! ", error));
-}
+// function loadHTML(elementId, file) {
+//   fetch(file)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       document.getElementById(elementId).innerHTML = data;
+//     })
+//     .catch((error) => alert("Lỗi trong quá trình tải và đọc dữ liệu! ", error));
+// }
 
-// Load header and footer
-window.onload = function () {
-  loadHTML("subNav", "/partials/subnav.html");
-  loadHTML("footer", "/partials/footer.html");
-  loadHTML("navbar", "/partials/navbar.html");
-};
+// // Load header and footer
+// window.onload = function () {
+//   loadHTML("footer", "/partials/footer.html");
+// };
